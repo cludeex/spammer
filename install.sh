9e6016deff66ac213fc28c4abb76a066bd16d8d7
milkpro <milkpro1337@gmail.com>
2020-04-14 20:45:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-clear
 pkg install python
 pkg install python3
 pkg install python3-pip
 pkg install dos2unix
 pip3 install requests
 pip3 install colorama
-cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/Spammer
+cp ~/Spammer/Spammer.py /data/data/com.termux/files/usr/bin/Spammer
 dos2unix /data/data/com.termux/files/usr/bin/Spammer
 chmod 777 /data/data/com.termux/files/usr/bin/Spammer
 Spammer
