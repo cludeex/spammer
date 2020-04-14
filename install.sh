591ffebae3024988fad90e9b70fd443164d5f8c3
milkpro <milkpro1337@gmail.com>
2020-04-14 20:57:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ pkg install python3-pip
 pkg install dos2unix
 pip3 install requests
 pip3 install colorama
-https://github.com/milkpro1337/Spammer
 cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer
 dos2unix /data/data/com.termux/files/usr/bin/spammer
 chmod 777 /data/data/com.termux/files/usr/bin/spammer
