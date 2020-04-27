d796b0895bbb0798bf071b8b467c490c1e4a7d0f
milkpro <milkpro1337@gmail.com>
2020-04-27 14:00:57 +0300

    INSTALL

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import os
-os.system('pip3 install requests')
-os.system('pip3 install colorama')
+os.system('pkg install dos2unix')
+os.system('pip install requests')
+os.system('pip install colorama')
 os.system('cp /data/data/com.termux/files/home/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer')
 os.system('dos2unix /data/data/com.termux/files/usr/bin/spammer')
 os.system('chmod 777 /data/data/com.termux/files/usr/bin/spammer')
