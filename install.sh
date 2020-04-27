dee32b663b3a8a62290afb9f633899f05cfe5d68
milkpro <milkpro1337@gmail.com>
2020-04-27 12:14:02 +0300

    INSTALL

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ pkg install python3-pip
 pkg install dos2unix
 pip3 install requests
 pip3 install colorama
-cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spamer
-dos2unix /data/data/com.termux/files/usr/bin/spamer
-chmod 777 /data/data/com.termux/files/usr/bin/spamer
-spamer
+cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer
+mv /data/data/com.termux/files/usr/bin/'spammer'$'\r' /data/data/com.termux/files/usr/bin/spammer
+dos2unix /data/data/com.termux/files/usr/bin/spammer
+chmod 777 /data/data/com.termux/files/usr/bin/spammer
+spammer
