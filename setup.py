2d755e685ffdda56e5fe4236d7d8d3352c1a4398
milkpro <milkpro1337@gmail.com>
2020-05-27 11:00:02 +0300

    SETUP

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+os.system('pkg install dos2unix')
+os.system('cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer')
+os.system('dos2unix /data/data/com.termux/files/usr/bin/spammer')
+os.system('chmod 777 /data/data/com.termux/files/usr/bin/spammer')
+os.system('chmod -R 777 ~/spammer')
+os.system('spammer')
