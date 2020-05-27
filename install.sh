4401bbdf38037f673596331501cc9f0583906f83
milkpro <milkpro1337@gmail.com>
2020-05-27 15:56:21 +0300

    INSTALL

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt install python
-apt install python3
-apt install python3-pip
+pkg install python
+pkg install python3
+pkg install python3-pip
 pip3 install requests
 pip3 install colorama
 pkg install dos2unix
