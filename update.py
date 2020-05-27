3b99cbe6c6108df32449dd6a3aa143debd8b53d9
milkpro <milkpro1337@gmail.com>
2020-05-27 15:57:32 +0300

    UPDATE

--- update.py
+++ update.py
@@ -1,2 +1,2 @@
 import os
-os.system("cd && rm -rf ~/Spammer && git clone https://github.com/milkpro1337/Spammer && python ~/Spammer/install.py")
+os.system("cd && rm -rf ~/Spammer && git clone https://github.com/milkpro1337/Spammer && sh ~/Spammer/install.sh")
