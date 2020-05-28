83c1e62d2de795264513aabc1ce319457b3eb28e
milkpro <milkpro1337@gmail.com>
2020-05-28 11:13:18 +0300

    SETUP

--- setup.py
+++ setup.py
@@ -2,5 +2,4 @@ import os
 os.system('cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer')
 os.system('dos2unix /data/data/com.termux/files/usr/bin/spammer')
 os.system('chmod 777 /data/data/com.termux/files/usr/bin/spammer')
-os.system('chmod -R 777 ~/spammer')
 os.system('spammer')
