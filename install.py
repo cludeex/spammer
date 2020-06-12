25040d92e5bc3292d2fdc65e4d2883eb653448a5
milkpro <milkpro1337@gmail.com>
2020-06-12 17:48:51 +0300

    INSTALL

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+os.system("pkg install python")
+os.system("pkg install dos2unix")
+os.system("pip install requests")
+os.system("cp ~/Spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer")
+os.system("dos2unix /data/data/com.termux/files/usr/bin/spammer")
+os.system("chmod -R 777 ~/spammer")
+os.system("chmod 777 /data/data/com.termux/files/usr/bin/spammer")
+os.system("spammer")
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkg install python
-pkg install dos2unix
-pip install requests
-cp '~/Spammer/spammer.py' '/data/data/com.termux/files/usr/bin/spammer'
-dos2unix '/data/data/com.termux/files/usr/bin/spammer'
-chmod -R 777 '~/spammer'
-chmod 777 '/data/data/com.termux/files/usr/bin/spammer'
-spammer
