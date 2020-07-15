cf8d0cb3fb8e24a7f3e7b090d6281e388f9d5048
cludeex <cludeex@gmail.com>
2020-07-15 12:22:46 +0800

    INSTALL

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import os
 os.system("pkg install dos2unix")
 os.system("pip install requests")
-os.system("cp ~/spamer/spamer.py /data/data/com.termux/files/usr/bin/spamer")
-os.system("dos2unix /data/data/com.termux/files/usr/bin/spamer")
-os.system("chmod 777 /data/data/com.termux/files/usr/bin/spamer")
-os.system("spamer")
+os.system("cp ~/spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer")
+os.system("dos2unix /data/data/com.termux/files/usr/bin/spammer")
+os.system("chmod 777 /data/data/com.termux/files/usr/bin/spammer")
+os.system("spammer")
