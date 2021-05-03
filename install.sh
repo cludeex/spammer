4d14683c1c52ccc8f1c51b593747e7c2eecd5c02
cludeex <cludeex@gmail.com>
2021-05-03 19:11:03 +0000

    Initial commit

--- install.sh
+++ install.sh
@@ -4,10 +4,7 @@ if [ -e  /usr/lib/sudo ]
 then
     if [ -e /usr/bin/apt-get ]
     then
-        sudo apt-get update
-        sudo apt-get upgrade -y
-        sudo apt-get install python3 -y
-        sudo apt-get install python3-pip -y
+        sudo apt-get install python3 python3-pip
         sudo cp spammer.py /usr/bin/spammer
         chmod +x /usr/bin/spammer
     fi
@@ -16,10 +13,7 @@ else
     then
         if [ -e /usr/bin/apt-get ]
         then
-            apt-get update
-            apt-get upgrade -y
-            apt-get install python3 -y
-            apt-get install python3-pip -y
+            apt-get install python3 python3-pip
             cp spammer.py /usr/bin/spammer
             chmod +x /usr/bin/spammer
         fi
@@ -27,10 +21,7 @@ else
 fi
 if [ -d /data/data/com.termux/files/usr/bin ]
 then
-    pkg update
-    pkg upgrade -y
-    pkg install python3 -y
-    pkg install python3-pip -y
+    pkg install python
     cp spammer.py $PREFIX/bin/spammer
     chmod +x $PREFIX/bin/spammer
 fi
@@ -38,10 +29,7 @@ if [ -e  /usr/lib/sudo ]
 then
     if [ -e /usr/bin/yum ]
     then
-        sudo yum update
-        sudo yum upgrade -y
-        sudo yum install python3 -y
-        sudo yum install python3-pip -y
+        sudo yum install python3 python3-pip
         sudo cp spammer.py /usr/bin/spammer
         chmod +x /usr/bin/spammer
     fi
@@ -50,10 +38,7 @@ else
     then
         if [ -e /usr/bin/yum ]
         then
-            yum update
-            yum upgrade -y
-            yum install python3 -y
-            yum install python3-pip -y
+            yum install python3 python3-pip
             cp spammer.py /usr/bin/spammer
             chmod +x /usr/bin/spammer
         fi
@@ -61,15 +46,13 @@ else
 fi
 if [ -e  /usr/local/bin/brew ]
 then
-    brew install python3 -y
-    brew install python3-pip -y
+    brew install python3 python3-pip
     cp spammer.py /usr/local/bin/spammer
     chmod +x /usr/local/bin/spammer
 fi
 if [ -e  /usr/bin/apk ]
 then
-    apk install python3 -y
-    apk install python3-pip -y
+    apk install python3
     cp spammer.py /usr/bin/spammer
     chmod +x /usr/bin/spammer
 fi
